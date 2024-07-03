1af34b484797238013d66424bdf9bdc3cb2f9164
mityaaa <burydima28122003@gmail.com>
2024-07-03 13:38:29 +0300

    add burger-menu logic and styles

--- vite-project/main.js
+++ vite-project/main.js
@@ -1,3 +1,24 @@
 import 'normalize.css'
-import './src/styles/base.scss'
 import './src/styles/style.scss'
+
+import Mmenu from 'mmenu-js'
+import 'mburger-webcomponent'
+
+
+console.log(document)
+document.addEventListener(
+    "DOMContentLoaded", () => {
+        new Mmenu("#mmenu", {
+            navbar: {
+                add: false
+            },
+            slidingSubmenus: false,
+            offCanvas: {
+                position: "right",
+                page: {
+                    selector: "#app",
+                }
+            },
+        });
+    }
+);
