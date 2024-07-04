4fa2c3bdcf585f21eceafb407951b2d11225cc28
mityaaa <burydima28122003@gmail.com>
2024-07-04 11:54:33 +0300

    add owl-carousel and creare tariffs__carousel
    also fix lh in .button

--- vite-project/main.js
+++ vite-project/main.js
@@ -4,8 +4,6 @@ import './src/styles/style.scss'
 import Mmenu from 'mmenu-js'
 import 'mburger-webcomponent'
 
-
-console.log(document)
 document.addEventListener(
     "DOMContentLoaded", () => {
         new Mmenu("#mmenu", {
@@ -20,5 +18,36 @@ document.addEventListener(
                 }
             },
         });
+        console.log(window.innerWidth)
     }
 );
+
+$(document).ready(function () {
+    $(".owl-carousel").owlCarousel({
+        nav:true,
+        navContainer: ".tariffs__buttons",
+        dotsContainer: ".tariffs__dots",
+        responsive:{
+            0:{
+                items:1,
+                margin:15,
+                stagePadding: 20,
+            },
+            590:{
+                items:2,
+                margin:20,
+                stagePadding: 30,
+            },
+            1330:{
+                items:4,
+                margin:20,
+                stagePadding: 1,
+            },
+            1440:{
+                items:4,
+                margin:30,
+                stagePadding: 1,
+            }
+        }
+    });
+});
